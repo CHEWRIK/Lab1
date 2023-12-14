91dcb1af20d08f575b8c703e000ed920b1bb5377
CHEWRIK <150577969+CHEWRIK@users.noreply.github.com>
2023-12-14 12:36:23 +0300

    Update main.cpp

--- lab1.1/main.cpp
+++ lab1.1/main.cpp
@@ -1,45 +1,62 @@
-#include <stdio.h>
-#include <iostream>
-#include <cctype>
-#include "BetaCipher.h"
-#include <locale>
+#include  "BetaCipher.hpp"
 using namespace std;
-bool isValid(const wstring& s)
+std::wstring toValid(std::wstring& s)
 {
-    for(auto c:s)
-        if (!iswalpha(c) || !iswupper(c))
-            return false;
-    return true;
+    //setup converter
+    using convert_type = std::codecvt_utf8<wchar_t>;
+    std::wstring_convert<convert_type, wchar_t> converter;
+
+    if (s.empty())
+        throw cipher_error("Empty text");
+
+    //use converter (.to_bytes: wstr->str, .from_bytes: str->wstr)
+    std::wstring tmp(s);
+    std::string st = converter.to_bytes(s);
+    for (auto & c:tmp) {
+        if (!iswalpha(c))
+            throw cipher_error(("Text has invalid symbols: ") +st);
+        if (iswlower(c))
+            c = towupper(c);
+    }
+    return tmp;
 }
 int main(int argc, char **argv)
 {
-    int key=3;
-    locale loc("ru_RU.UTF-8");
-    locale::global(loc);
-    wstring z;
-    wstring text;
-    unsigned op;
-    wcout<<L"Cipher ready. Input please Password: ";
-    wcin>>z;
-    BetaCipher cipher(key);
-    do {
-        wcout<<L"Cipher ready. Input please operation (0-exit, 1-encrypt, 2-decrypt): ";
-        wcin>>op;
-        if (op > 2) {
-            wcout<<L"Illegal operation\n";
-        } else if (op >0) {
-            wcout<<L"Cipher ready. Input please text: ";
-            wcin>>text;
-            if (isValid(text)) {
-                if (op==1) {
-                    wcout<<L"Encrypted text: "<<cipher.CoderBetaCipher(cipher, text)<<endl;
-                } else {
-                    wcout<<L"Decrypted text: "<<cipher.DecoderBetaCipher(cipher, text)<<endl;
-                }
-            } else {
-                wcout<<L"Operation aborted: invalid text\n";
-            }
+    try {
+        locale loc("ru_RU.UTF-8");
+        locale::global(loc);
+        int key;
+        wstring text;
+        int op;
+        wcout<<L"Cipher ready. Input key: ";
+        wcin>>key;
+        if (!wcin.good()) {
+            throw cipher_error("key not valid\n");
         }
-    } while (op!=0);
-    return 0;
+
+        BetaCipher cipher(key);
+        do {
+            wcout<<L"Cipher ready. Input operation (0-exit, 1-encrypt, 2-decrypt): ";
+            wcin>>op;
+            if (op > 2) {
+                throw cipher_error("Illegal operation\n");
+            } else if (op >0) {
+                wcout<<L"Cipher ready. Input text: ";
+                wcin>>text;
+                std::wstring vtext=toValid(text);
+                if (op==1) {
+                    wcout<<L"Encrypted text: "<<cipher.encrypt(vtext)<<endl;
+                } else {
+                    wcout<<L"Decrypted text: "<<cipher.decrypt(vtext)<<endl;
+                }
+            }
+        } while (op!=0);
+    } catch (const cipher_error& e) {
+        cerr << "Error: " << e.what() << endl;
+        return 1;
+    } catch (const exception& e) {
+        cerr << "Exception: " << e.what() << endl;
+        return 1;
+
+    }
 }
